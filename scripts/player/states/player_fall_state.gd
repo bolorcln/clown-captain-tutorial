8e506d71b564cf1be20cf258ba49d6660b5bf80a
bolor <bolorcln@gmail.com>
2024-06-20 13:26:02 +0800

    player init

---
+++ scripts/player/states/player_fall_state.gd
@@ -0,0 +1,21 @@
+extends PlayerBaseState
+
+@onready var coyote_timer = $CoyoteTimer
+
+func enter():
+	play("fall")
+	if fsm.previous_state != "jump":
+		coyote_timer.start()
+
+func physics_update(delta):
+	move(delta, true)
+	
+	if not coyote_timer.is_stopped() and input.jump_just_pressed:
+		change_state("jump")
+	elif object.is_on_floor():
+		if input.jump_buffer:
+			change_state("jump")
+		elif input.x == 0:
+			change_state("idle")
+		else:
+			change_state("run")
